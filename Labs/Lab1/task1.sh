fb5ba0460472bd33ea7a7a4a743ad152a134a25a
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-02-25 23:20:42 +0300

    Create task1.sh

---
+++ Labs/Lab1/task1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "requires you to enter 3 arguments you have entered $#"
+    exit 1
+fi
+
+max=-239
+
+for i in "$@"; do
+    if [ "$i" -gt "$max" ]; then
+        max=$i
+    fi
+done
+
+echo "Max is $max"
