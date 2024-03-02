153c9f1e47716a5f309a6385a499d811626b9ec3
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-03-02 12:34:32 +0300

    Create task.bash

---
+++ Labs/Lab1/task.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+file_name=$1
+
+if [[ ! -f "$file_name" ]]; then
+    echo "No such file"
+    exit 1
+fi
+
+if [[ $file_name =~ [^a-zA-Z0-9._-] ]]; then
+    echo "Invalid characters in file name"
+    exit 2
+fi
+
+sed '/#TEMPLATE/d' $file_name > "${file_name%.*}.sh"
+sed -i '1i #!/bin/bash' "${file_name%.*}.sh"
+
+trap 'rm -f "${file_name%.*}.sh" exit 1' INT
+
+echo "Press ^C to cancel the installation"
+sleep 10
+echo -e "\033[2K\033[1A\033[2K"
