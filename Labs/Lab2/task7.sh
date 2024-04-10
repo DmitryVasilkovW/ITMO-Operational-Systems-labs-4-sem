7edb938dd11d2b8f8a346f618ee298b0d26ad1b4
Dmitry Vasilkov <113594620+DmitryVasilkovW@users.noreply.github.com>
2024-04-10 22:01:39 +0300

    Update task7.sh

--- Labs/Lab2/task7.sh
+++ Labs/Lab2/task7.sh
@@ -5,8 +5,8 @@ function info() {
   proc_info=$(ps -Ao pid,cmd | tail -n +2 | awk ' {print $1": "$2}')
   
   for i in $proc_info; do 
-    pid=$(echo $i | awk -F ":" ' {print $1}') 
-    com=$(echo $i | awk -F ":" ' {print $2}') 
+    pid=$(echo $i | awk -F ":" '{print $1}') 
+    com=$(echo $i | awk -F ":" '{print $2}') 
     path="/proc/"$pid
     
     if [ -f $path/io ]; then
